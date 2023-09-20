2887c6e458cc5cd9e70be2346aa1dcbcfb61bbbe
Joris Pinkse <pinkse@gmail.com>
2023-09-20 11:37:12 -0400

    fixed tests

--- test/test_luschny.jl
+++ test/test_luschny.jl
@@ -1,10 +1,10 @@
 
 
 @testset "Luschny" begin
-    for x ∈ [1.0, 2.0, 4.0 ]
-        @test LuschnyGamma( x ) ≈ x * LuschnyGamma( x - 1 )
+    for x ∈ [2.0, 2.0, 8.0 ]
+        @test LuschnyFactorial( x ) ≈ x * LuschnyFactorial( x - 1 )
     end
-    @test LuschnyGamma( x ) ≈ 0.5
-    @test LuschnyGamma(8.0) ≈ 40_320.0
+    @test LuschnyFactorial( 0.0 ) ≈ 0.5
+    @test LuschnyFactorial(8.0) ≈ 40_320.0
 end
 
