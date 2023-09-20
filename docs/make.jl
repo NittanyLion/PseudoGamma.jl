7a25b0618b8708f2e2dd4ab53fbbcb9afc5095ed
Joris Pinkse <pinkse@gmail.com>
2023-09-19 23:44:02 -0400

    updated make

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,29 @@
 using PseudoGamma
 using Documenter
 
-DocMeta.setdocmeta!(PseudoGamma, :DocTestSetup, :(using PseudoGamma); recursive=true)
+# DocMeta.setdocmeta!(PseudoGamma, :DocTestSetup, :(using PseudoGamma); recursive=true)
 
-makedocs(;
-    modules=[PseudoGamma],
-    authors="Joris Pinkse <pinkse@gmail.com> and contributors",
-    repo="https://github.com/NittanyLion/PseudoGamma.jl/blob/{commit}{path}#{line}",
-    sitename="PseudoGamma.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://NittanyLion.github.io/PseudoGamma.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+# makedocs(;
+#     modules=[PseudoGamma],
+#     authors="Joris Pinkse <pinkse@gmail.com> and contributors",
+#     repo="https://github.com/NittanyLion/PseudoGamma.jl/blob/{commit}{path}#{line}",
+#     sitename="PseudoGamma.jl",
+#     format=Documenter.HTML(;
+#         prettyurls=get(ENV, "CI", "false") == "true",
+#         canonical="https://NittanyLion.github.io/PseudoGamma.jl",
+#         edit_link="main",
+#         assets=String[],
+#     ),
+#     pages=[
+#         "Home" => "index.md",
+#     ],
+# )
+
+makedocs( sitename = "PseudoGamma.jl",
+    authors = "Joris Pinkse",
+    pages = [
+        "Home" => "index.md"
+    ]
 )
 
 deploydocs(;
